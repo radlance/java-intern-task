9227115f83f9fa630297481ffb4428a614df51c0
Dmitry <manyakindima@gmail.com>
2024-10-16 19:23:02 +0300

    Initial commit

---
+++ src/main/java/com/radlance/javainterntask/JavaInternTaskApplication.java
@@ -0,0 +1,13 @@
+package com.radlance.javainterntask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radlance/javainterntask/JavaInternTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radlance.javainterntask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
