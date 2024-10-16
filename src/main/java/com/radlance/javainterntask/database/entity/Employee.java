c2c7faf8c83819779d65595eb757d33f8ec3cad8
Dmitry <manyakindima@gmail.com>
2024-10-16 20:41:11 +0300

    add Employee table

---
+++ src/main/java/com/radlance/javainterntask/database/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.radlance.javainterntask.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    private String department;
+    private Double salary;
+}
